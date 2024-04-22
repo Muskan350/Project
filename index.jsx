47f7a38b43fc382b525ddaece248c4ab58bcdc41
Muskan350 <155904606+Muskan350@users.noreply.github.com>
2024-04-22 12:54:16 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,25 +1,23 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../components/recipe-item";
 
-export default function RecipeItem({ item }) {
+export default function Home() {
+  const { recipeList, loading } = useContext(GlobalContext);
+
+  if (loading) return <div>Loading...Please wait!</div>;
+
   return (
-    <div className="flex flex-col w-80 overflow-hidden p-5 bg-white/75 shadow-xl gap-5 border-2 rounded-2xl border-white">
-      <div className="h-40 flex justify-center overflow-hidden items-center rounded-xl">
-        <img src={item?.image_url} alt="recipe item" className="block w-full" />
-      </div>
-      <div>
-        <span className="text-sm text-cyan-700 font-medium">
-          {item?.publisher}
-        </span>
-        <h3 className="font-bold text-2xl truncate text-black">
-          {item?.title}
-        </h3>
-        <Link
-          to={`/recipe-item/${item?.id}`}
-          className="text-sm p-3 mt-5 px-8 rounded-lg uppercase font-medium tracking-wider inline-block shadow-md bg-black text-white"
-        >
-          Recipe Details
-        </Link>
-      </div>
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+            Nothing to show. Please search something
+          </p>
+        </div>
+      )}
     </div>
   );
 }
